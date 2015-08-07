2b9347704c31e8bfcaa57759c33f0211f1eb7cac
Gergely Imreh <imrehg@gmail.com>
2015-08-07 12:24:39 +0800

    Add distance calculation function

--- Dorothy.ino
+++ Dorothy.ino
@@ -1,6 +1,10 @@
 #include <Wire.h>
 #include <rgb_lcd.h>
 #include <LGPS.h>
+#include <Math.h>
+
+#define R 6371000
+#define TO_RAD (3.1415926536 / 180)
 
 gpsSentenceInfoStruct info;
 char buff[256];
@@ -94,6 +98,18 @@ static double getDecimalPos(const char *str)
   return pos;
 }
 
+/* From Rosetta Code: http://rosettacode.org/wiki/Haversine_formula#C */
+double getGreatCircleDistance(double th1, double ph1, double th2, double ph2)
+{
+	double dx, dy, dz;
+	ph1 -= ph2;
+	ph1 *= TO_RAD, th1 *= TO_RAD, th2 *= TO_RAD;
+
+	dz = sin(th1) - sin(th2);
+	dx = cos(ph1) * cos(th1) - cos(th2);
+	dy = sin(ph1) * cos(th1);
+	return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
+}
 
 void parseGPGGA(const char* GPGGAstr)
 {
